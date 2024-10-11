e20ec06668363961bc2dd7bf0176dc99f18dbf2a
yeongie <ilee002@ucr.edu>
2024-10-11 10:39:04 -0700

    Changed variables to c, this will cause issues because we altered original code shared between the branches

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int a = 3;
-    std::cout << a << ": doing this should cause issues" << std::endl;
+    int c = 3;
+    std::cout << c << ": doing this should cause issues" << std::endl;
     std::cout << "Hi" << std::endl;
     return 0;
 }
