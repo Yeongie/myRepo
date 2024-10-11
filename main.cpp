33901ef6eaa346568ccc10dc17e3bdb733468fb1
yeongie <ilee002@ucr.edu>
2024-10-11 10:37:00 -0700

    Added a harmless line

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     int a = 3;
     std::cout << a << ": doing this should cause issues" << std::endl;
+    std::cout << a << ": This shouldn't hurt anyone" << std::endl;
     std::cout << "Hi" << std::endl;
     return 0;
 }
